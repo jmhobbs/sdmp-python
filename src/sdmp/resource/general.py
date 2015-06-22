bc9f8819d062fc4e9089ff95e9bbabac9cac1bdd
John Hobbs <john@velvetcache.org>
2015-06-22 11:29:13 -0500

    General document types.

--- src/sdmp/resource/general.py
+++ src/sdmp/resource/general.py
@@ -5,6 +5,7 @@ class GeneralDocument (Resource):
 
     def __init__(self):
         super(GeneralDocument, self).__init__()
+        self.document = {}
 
 
 class UserInformationDocument (GeneralDocument):
@@ -13,3 +14,18 @@ class UserInformationDocument (GeneralDocument):
         super(UserInformationDocument, self).__init__()
         self.user_name = None
         self.user_about = None
+
+class NodeInformationDocument (GeneralDocument):
+
+    def __init__(self):
+        super(NodeInformationDocument, self).__init__()
+        self.node_name = None
+        self.node_ips = []
+
+
+class EncryptedContentDocument (GeneralDocument):
+
+    def __init__(self):
+        super(EncryptedContentDocument, self).__init__()
+        self.encrypted_keys = []
+        self.encrypted_data = []
